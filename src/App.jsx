fe6242726afe8f15907d1f061d1cca52e2574d07
wangbertha <wangbertha@gmail.com>
2024-09-23 12:18:44 -0500

    Add and render puppy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+import { puppyList } from './data'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
 
   return (
     <>
-
+{puppies.map((puppy) => {
+  return <p key={puppy.id}>{puppy.name}</p>
+})}
     </>
   )
 }
