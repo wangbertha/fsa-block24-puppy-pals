d79bcd78ad32a8d6655f36b2c3d0d0cbf7de8006
wangbertha <wangbertha@gmail.com>
2024-09-23 12:41:03 -0500

    Add functionality to select and display single pup

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <>
-{puppies.map((puppy) => {
-  return <p key={puppy.id}>{puppy.name}</p>
-})}
+      {puppies.map((puppy) => {
+        return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   )
 }
